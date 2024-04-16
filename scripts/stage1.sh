f280816a3789076be81d37ea9f18fc4d8121822c
Zener085 <zener.085@mail.ru>
2024-04-16 10:35:43 +0300

    updated stage 1 and start of stage 2

--- scripts/stage1.sh
+++ scripts/stage1.sh
@@ -1,1 +1,21 @@
 #!/bin/bash
+
+password=$(head -n 1 bigdata-final-project/secrets/.psql.pass)
+
+pip install kaggle 
+
+kaggle datasets download -d mkechinov/ecommerce-behavior-data-from-multi-category-store/ -p bigdata-final-project/data/
+
+unzip bigdata-final-project/data/ecommerce-behavior-data-from-multi-category-store.zip -d bigdata-final-project/data/
+
+rm bigdata-final-project/data/ecommerce-behavior-data-from-multi-category-store.zip
+
+python3 bigdata-final-project/scripts/build_db.py
+
+hdfs dfs -rm -r /user/team16/project/warehouse/*
+
+sqoop import-all-tables --connect jdbc:postgresql://hadoop-04.uni.innopolis.ru/team16_projectdb --username team16 --password $password --compression-codec=snappy --compress --as-avrodatafile --warehouse-dir=project/warehouse --m 1
+
+# ?
+mv *.avsc bigdata-final-project/output/
+mv *.java bigdata-final-project/output/
--- scripts/stage2.sh
+++ scripts/stage2.sh
@@ -1,1 +1,10 @@
 #!/bin/bash
+
+hdfs dfs -mkdir -p project/warehouse/avsc
+hdfs dfs -put bigdata-final-project/output/*.avsc project/warehouse/avsc
+
+password=$(head -n 1 bigdata-final-project/secrets/.psql.pass)
+
+beeline -u jdbc:hive2://hadoop-03.uni.innopolis.ru:10001 -n team16 -p $password -f bigdata-final-project/sql/db.hql > bigdata-final-project/output/hive_results.txt 2> /dev/null
+
+beeline -u jdbc:hive2://hadoop-03.uni.innopolis.ru:10001 -n team16 -p $password -f bigdata-final-project/sql/partition.hql
